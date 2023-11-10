b6d14a42a00bd0f98b3c4776beee697a217e77f1
Safiullah Omar <omar.safiullah@gmail.com>
2023-11-10 18:23:15 +0100

    email validation

--- expense_project/authentication/views.py
+++ expense_project/authentication/views.py
@@ -17,5 +17,16 @@ class UsernameValidationView(View):
         if User.objects.filter(username=username).exists():
             return JsonResponse({'username_error': 'sorry username in use,choose another one '}, status=409)
         return JsonResponse({'username_valid':True})
+    
+    
+class UsernameValidationView(View):
+    def post(self,request):
+        data=json.loads(request.body)
+        email=data['email']
+        if not str(email).isalnum():
+            return JsonResponse({'email_error': 'email should only contain alphanumeric characters'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'sorry email in use,choose another one '}, status=409)
+        return JsonResponse({'email_valid':True})
 
 # Create your views here.
