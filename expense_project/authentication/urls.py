656493895d85945672847f15c25fed6e274edbfc
Safiullah Omar <omar.safiullah@gmail.com>
2023-11-10 17:55:22 +0100

    ajax validation

--- expense_project/authentication/urls.py
+++ expense_project/authentication/urls.py
@@ -1,6 +1,8 @@
-from .views import registrationView
+from .views import registrationView,UsernameValidationView
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('register',registrationView.as_view(),name="register")
+    path('register',registrationView.as_view(),name="register"),
+    path('validation-username',csrf_exempt(UsernameValidationView.as_view()),name="validation-username")
 ]
